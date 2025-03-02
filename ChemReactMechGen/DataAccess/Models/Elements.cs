4a1a2a7ebd928998cc76b47ca08a92f02165daff
4n0u22 <148545954+4n0u22@users.noreply.github.com>
2025-03-02 04:20:43 +0300

    Initial
    First models:
    Частица, Электрон, Ядро (протон-нейтрон)

---
+++ ChemReactMechGen/DataAccess/Models/Elements.cs
@@ -0,0 +1,8 @@
+﻿namespace DataAccess.Models;
+
+public class Elements
+{
+}
+public partial class ЭлекронныйУровень
+{
+}
---
+++ ChemReactMechGen/DataAccess/Models/Particals.cs
@@ -0,0 +1,26 @@
+﻿namespace DataAccess.Models;
+
+public abstract partial class Частица
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public abstract double Масса { get; set; }
+    public abstract sbyte Спин { get; set; }
+    public abstract sbyte Заряд { get; }
+    public abstract byte КолвоНесущихЗаряд { get; set; }
+
+}
+public sealed partial class Электрон : Частица
+{
+    public override double Масса { get; set; } = 9.10938356e-31; // Масса электрона в кг
+    public override sbyte Спин { get; set; }
+    public override sbyte Заряд => -1;
+    public override byte КолвоНесущихЗаряд { get; set; } = 0;
+}
+public sealed partial class Ядро : Частица
+{
+    public override double Масса { get; set; } = 1.6726219e-27; // Масса протона в кг
+    public override sbyte Спин { get; set; }
+    public override sbyte Заряд => 1;
+    public override byte КолвоНесущихЗаряд { get; set; } = 1;
+    public int КолвоЧастиц { get; set; } = 1;
+}
